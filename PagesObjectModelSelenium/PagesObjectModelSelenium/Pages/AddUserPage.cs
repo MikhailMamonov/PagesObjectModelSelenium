1b96619d00bf560d4454159b6cc3fe0cce950116
MikhailMamonov <mamon201071@gmail.com>
2025-11-09 13:44:49 +0300

    [dev-4] - AddUserPage reverted

--- PagesObjectModelSelenium/PagesObjectModelSelenium/Pages/AddUserPage.cs
+++ PagesObjectModelSelenium/PagesObjectModelSelenium/Pages/AddUserPage.cs
@@ -4,37 +4,7 @@ using OpenQA.Selenium;
 
 namespace PagesObjectModelSelenium.Pages;
 
-public class AddUserPage : BasePage
-{
-    private readonly By _usernameTextBox = By.Id("username");
-    private readonly By _passwordTextBox = By.Id("password");
-    private readonly By _signInButton = By.Id("kc-login");
-
-    public AddUserPage(IWebDriver driver) : base(driver)
-    {
-
-    }
-
-    public void SetUsername(string username)
-    {
-        SendKeys(_usernameTextBox,username);
-    }
-
-    public void SetPassword(string password)
-    {
-        SendKeys(_passwordTextBox,password);
-    }
-
-    public void ClickSignInButton()
-    {
-        Click(_signInButton);
-    }
-
-    public void Login(string email,string password)
-    {
-        SetUsername(email);
-        SetPassword(password);
-        ClickSignInButton();
-    }
+public class AddUserPage 
+{ 
 }
 
